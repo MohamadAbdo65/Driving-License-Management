ede9b0882d5fec42ced7e81dbf132398deaf34c7
Mohamad <www.modi65@gmail.com>
2025-03-05 16:58:06 +0200

    Add API project

---
+++ DVLD_API/Controllers/clsApplicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DVLD_API.Controllers
+{
+    [Route("api/ApplicationsAPI")]
+    [ApiController]
+    public class clsApplicationController : ControllerBase
+    {
+
+
+    }
+}
